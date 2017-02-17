0c9ff3c24464e983cb35bbff4c50a140160d2bd8
harley1011 <harley.1011@gmail.com>
2017-02-16 23:04:12 -0500

    Adding all non-terminal procedures

--- src/SyntaxParser/SyntaxParser.h
+++ src/SyntaxParser/SyntaxParser.h
@@ -20,60 +20,54 @@ class SyntaxParser {
 
     string next_token();
     bool parse(vector<Token*> tokens);
+    bool match(string token);
 
     bool prog();
     bool classDeclLst();
-    bool classDec();
-
-    bool match();
-
-    bool match(string token);
-
     bool classDecl();
-
     bool classBody();
-
     bool classInDecl();
-
     bool postTypeId();
-
     bool progBody();
-
     bool funcHead();
-
     bool funcDefLst();
-
     bool funcDef();
-
     bool funcBody();
-
-    static bool arithExpr;
-public:
-    bool multOp();
-
-    bool multOp();
-
-    bool addOp();
-
-    bool relOp();
-
-    bool assignOp();
-
-    bool num();
-
-    bool aParamsTail();
-
-    bool fParamsTail();
-
-    bool aParams();
-
-    bool fParams();
-
-    bool numType();
-
-    bool arraySize();
-
+    bool funcInBodyLst();
+    bool funcInBody();
+    bool varOrStat();
+    bool statementLst();
+    bool statement();
+    bool statementRes();
+    bool assignStat();
+    bool statBlock();
+    bool expr();
+    bool relOrAri();
+    bool arithExpr();
+    bool arithExprD();
+    bool sign();
+    bool term();
+    bool termD();
+    bool factor();
+    bool factorVarOrFunc();
+    bool varOrFuncIdNest();
+    bool variable();
+    bool variableIndice();
+    bool idnest();
+    bool indice();
     bool indiceLst();
+    bool arraySize();
+    bool type();
+    bool numType();
+    bool fParams();
+    bool aParams();
+    bool fParamsTail();
+    bool aParamsTail();
+    bool assignOp();
+    bool relOp();
+    bool addOp();
+    bool multOp();
+    bool num();
 };
 
 
