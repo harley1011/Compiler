28b424caa362a696955e194aabfe77e53c85e7d2
harley1011 <harley.1011@gmail.com>
2017-02-16 22:26:59 -0500

    Adding SyntaxParser

--- src/SyntaxParser/SyntaxParser.cpp
+++ src/SyntaxParser/SyntaxParser.cpp
@@ -4,12 +4,200 @@
 #include "SyntaxParser.h"
 
 
+SyntaxParser::SyntaxParser() {
+
+    _first_sets["prog"] = {"class", "program"};
+}
 
 bool SyntaxParser::parse(vector<Token*> tokens) {
     _current_token_position = 0;
     _tokens = tokens;
 
     return prog();
+}
+
+
+bool SyntaxParser::prog() {
+    if ( _lookahead == "class" ) {
+
+
+    } else if ( _lookahead == "program" ) {
+
+    }
+}
+
+bool SyntaxParser::classDeclLst() {
+
+}
+
+bool SyntaxParser::classDecl() {
+
+}
+bool SyntaxParser::classBody() {
+
+}
+
+bool SyntaxParser::classInDecl() {
+
+}
+bool SyntaxParser::postTypeId() {
+
+}
+
+bool SyntaxParser::progBody() {
+
+}
+bool SyntaxParser::funcHead() {
+
+}
+
+bool SyntaxParser::funcDefLst() {
+
+}
+bool SyntaxParser::funcDef() {
+
+}
+
+bool SyntaxParser::funcBody() {
+
+}
+
+
+bool SyntaxParser::funcInBodyLst() {
+
+}
+
+bool SyntaxParser::funcInBody() {
+
+}
+
+bool SyntaxParser::varOrStat() {
+
+}
+
+bool SyntaxParser::statementLst() {
+
+}
+
+bool SyntaxParser::statement() {
+
+}
+
+bool SyntaxParser::statementRes() {
+
+}
+bool SyntaxParser::assignStat() {
+
+}
+bool SyntaxParser::statBlock() {
+
+}
+bool SyntaxParser::expr() {
+
+}
+bool SyntaxParser::relOrAri() {
+
+}
+bool SyntaxParser::arithExpr() {
+
+}
+bool SyntaxParser::arithExprD() {
+
+}
+bool SyntaxParser::sign() {
+
+}
+bool SyntaxParser::term() {
+
+}
+bool SyntaxParser::termD() {
+
+}
+bool SyntaxParser::factor() {
+
+}
+bool SyntaxParser::factorVarOrFunc() {
+
+}
+bool SyntaxParser::varOrFuncIdNest() {
+
+}
+bool SyntaxParser::variable() {
+
+}
+bool SyntaxParser::variableIndice() {
+
+}
+
+bool SyntaxParser::idnest() {
+
+}
+
+bool SyntaxParser::indice() {
+
+}
+
+bool SyntaxParser::indiceLst() {
+
+}
+
+
+bool SyntaxParser::arraySize() {
+
+}
+
+bool SyntaxParser::type() {
+
+}
+
+bool SyntaxParser::numType() {
+
+}
+bool SyntaxParser::fParams() {
+
+}
+
+bool SyntaxParser::aParams() {
+
+}
+bool SyntaxParser::fParamsTail() {
+
+}
+
+bool SyntaxParser::aParamsTail() {
+
+}
+bool SyntaxParser::assignOp() {
+
+}
+
+bool SyntaxParser::relOp() {
+
+}
+bool SyntaxParser::addOp() {
+
+}
+
+bool SyntaxParser::multOp() {
+
+}
+
+
+bool SyntaxParser::num() {
+
+}
+bool SyntaxParser::match(string token) {
+    if (_lookahead == token) {
+        _lookahead = next_token();
+        return (true);
+    }
+    else
+        _lookahead = next_token();
+    return false;
+}
+
+bool check_if_list_has_string(vector<string> tokens, string token) {
+
 }
 
 string SyntaxParser::next_token() {
--- src/SyntaxParser/SyntaxParser.h
+++ src/SyntaxParser/SyntaxParser.h
@@ -10,16 +10,70 @@
 
 class SyntaxParser {
 
+    SyntaxParser();
+
     string _lookahead;
     vector<Token*> _tokens;
     int _current_token_position;
+
+    map<string, vector<string>> _first_sets;
 
     string next_token();
     bool parse(vector<Token*> tokens);
 
     bool prog();
     bool classDeclLst();
+    bool classDec();
 
+    bool match();
+
+    bool match(string token);
+
+    bool classDecl();
+
+    bool classBody();
+
+    bool classInDecl();
+
+    bool postTypeId();
+
+    bool progBody();
+
+    bool funcHead();
+
+    bool funcDefLst();
+
+    bool funcDef();
+
+    bool funcBody();
+
+    static bool arithExpr;
+public:
+    bool multOp();
+
+    bool multOp();
+
+    bool addOp();
+
+    bool relOp();
+
+    bool assignOp();
+
+    bool num();
+
+    bool aParamsTail();
+
+    bool fParamsTail();
+
+    bool aParams();
+
+    bool fParams();
+
+    bool numType();
+
+    bool arraySize();
+
+    bool indiceLst();
 };
 
 
