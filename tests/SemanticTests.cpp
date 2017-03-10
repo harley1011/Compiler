143c4f1317a2b79acafb6c715602ed5f75f506a1
harley1011 <harley.1011@gmail.com>
2017-03-09 21:42:57 -0500

    Changing record to be passed by reference

--- tests/SemanticTests.cpp
+++ tests/SemanticTests.cpp
@@ -15,6 +15,22 @@ TEST(arraySizeTest, SemanticTests) {
     EXPECT_EQ(record->type_, "[5][10]");
 }
 
+TEST(FuncDeclareClassTest, SemanticTests)
+{
+    string find_min_func_string = "int findMin(int array[100]) { int minValue; int idx; minValue = array[100]; for( int idx = 1; idx <= 99; idx = ( idx ) + 1) { if(array[idx] < maxValue) then { maxValue = array[idx]; }else{}; }; return (minValue); }; ";
+    vector<Token*> tokens;
+    Scanner scanner;
+    tokens = scanner.generate_tokens("class nameHere { " + find_min_func_string + "}; program { };", false);
+
+    SemanticParser syntaxParser;
+
+    EXPECT_EQ(syntaxParser.parse(tokens), true);
+    EXPECT_EQ(syntaxParser.current_rhs_derivation_, "class id { int id ( int id [ integer ] ) { int id ; int id ; id = id [ integer ] ; for ( int id = integer ; id <= integer ; id = ( id ) + integer ) { if ( id [ id ] < id ) then { id = id [ id ] ; } else { } ; } ; return ( id ) ; } ; } ; program { } ;");
+    EXPECT_EQ(syntaxParser.errors_.size(), 0);
+}
+
+
+
 SemanticParser common_setup_semantic(string test_program, string derivation_string) {
     vector<Token*> tokens;
     Scanner scanner;
