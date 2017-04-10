d926c18fdb909cdba5a64a679398f2a97a730f8a
harley1011 <harley.1011@gmail.com>
2017-04-10 08:58:27 -0400

    Working on code_prog

--- moon/program_out.m
+++ moon/program_out.m
@@ -1,63 +1,198 @@
 program entry
 addi r13,r0,stack
-addi r13,r13,4
-
-addi r2,r0,10
-addi r1,r0,12
+addi r13,r13,40 % allocate stack memory for program
+addi r1,r0,5 % load integer value
+subi r12,r13,40 % load stack variable offset
+sw 0(r12),r1 % store register value into stack variable
+addi r1,r0,5 % load integer value
+subi r12,r13,16 % load stack variable offset
+sw 0(r12),r1 % store register value into stack variable
+addi r1,r0,0 % load integer value
+subi r12,r13,8 % load stack variable offset
+sw 0(r12),r1 % store register value into stack variable
+for0
+ % ---evaluating LT expression below ---
+subi r12,r13,8
+lw r1,0(r12) % load stack variable value in function
+add r2,r1,r0
+subi r8,r8,4
+sw topaddr(r8),r2 % store left hand side of relational expression on stack
+addi r1,r0,5 % load integer value
+lw r2,topaddr(r8)
+addi r8,r8,4
+clt r1,r2,r1
+ % ---evaluating LT expression over ---
+bz r1,forend0
+ % ---evaluating ADD expression below ---
+subi r12,r13,8
+lw r2,0(r12) % load stack variable value in function
+addi r1,r0,1 % load integer value
 add r1,r2,r1
 subi r8,r8,4
 sw topaddr(r8),r1
-
-
-addi r2,r0,5
+ % ---evaluting ADD expression over ---
 lw r1,topaddr(r8)
-addi r8,r8,4
-mul r1,r2,r1
-subi r8,r8,4
-sw topaddr(r8),r1
-
-
-lw r2,topaddr(r8)
-addi r8,r8,4
-addi r1,r0,200
-sub r1,r2,r1
-subi r8,r8,4
-sw topaddr(r8),r1
-
-
-addi r2,r0,112
-addi r1,r0,21
-add r1,r2,r1
-subi r8,r8,4
-sw topaddr(r8),r1
-
-
-lw r2,topaddr(r8)
-addi r8,r8,4
-addi r1,r0,32
-mul r1,r2,r1
-subi r8,r8,4
-sw topaddr(r8),r1
-
-
-lw r2,topaddr(r8)
-addi r8,r8,4
-lw r1,topaddr(r8)
-addi r8,r8,4
-add r1,r2,r1
-subi r8,r8,4
-sw topaddr(r8),r1
-
-lw r1,topaddr(r8)
-addi r8,r8,4
+addi r8,r8,4 % remove stack variable from arithmetic expression
+subi r12,r13,8 % load stack variable offset
+sw 0(r12),r1 % store register value into stack variable
+subi r12,r13,8
+lw r1,0(r12) % load stack variable value in function
+add r5,r0,r1
+ % --- compute index value ---
+add r9,r0,r0 % clear array index register
+add r7,r0,r0
+subi r12,r13,8
+lw r1,0(r12) % load stack variable value in function
+muli r7,r7,5
+muli r1,r1,4 % multiply last evaluated expression stored in r1 by the arrays type size
+add r7,r7,r1 % add new dimension access to current offset counter
+add r9,r9,r7 % add array dimension calculation with other properties
+ % --- compute index over ---
+subi r12,r13,36 % load stack array offset
+add r12,r12,r9
+sw 0(r12),r5 % store register value in stack array index
+j for0
+forend0
+subi r1,r13,36 % load stack array address
+addi r12,r13,4 % loading parameter offset
+sw 0(r12),r1 % storing value in function parameter stack
+subi r12,r13,40
+lw r1,0(r12) % load stack variable value in function
+addi r12,r13,404 % loading parameter offset
+sw 0(r12),r1 % storing value in function parameter stack
+subi r12,r13,16
+lw r1,0(r12) % load stack variable value in function
+addi r12,r13,408 % loading parameter offset
+sw 0(r12),r1 % storing value in function parameter stack
+ % ---- function call code beg----
+addi r13,r13,20 % allocate stack memory for function calls variables and parameters
+jl r11,func0
+subi r13,r13,20 % remove declared stack memory for function
+add r1,r1,r0 % store return value in register
+ % ---- function call code end ----
+subi r12,r13,12 % load stack variable offset
+sw 0(r12),r1 % store register value into stack variable
+addi r1,r0,0 % load integer value
+subi r12,r13,4 % load stack variable offset
+sw 0(r12),r1 % store register value into stack variable
+for1
+ % ---evaluating LT expression below ---
 subi r12,r13,4
-sw 0(r12),r1
+lw r1,0(r12) % load stack variable value in function
+add r2,r1,r0
+subi r8,r8,4
+sw topaddr(r8),r2 % store left hand side of relational expression on stack
+addi r1,r0,5 % load integer value
+lw r2,topaddr(r8)
+addi r8,r8,4
+clt r1,r2,r1
+ % ---evaluating LT expression over ---
+bz r1,forend1
+ % ---evaluating ADD expression below ---
 subi r12,r13,4
-lw r1,0(r12)
+lw r2,0(r12) % load stack variable value in function
+addi r1,r0,1 % load integer value
+add r1,r2,r1
+subi r8,r8,4
+sw topaddr(r8),r1
+ % ---evaluting ADD expression over ---
+lw r1,topaddr(r8)
+addi r8,r8,4 % remove stack variable from arithmetic expression
+subi r12,r13,4 % load stack variable offset
+sw 0(r12),r1 % store register value into stack variable
+ % --- compute index value ---
+add r9,r0,r0 % clear array index register
+add r7,r0,r0
+subi r12,r13,4
+lw r1,0(r12) % load stack variable value in function
+muli r7,r7,5
+muli r1,r1,4 % multiply last evaluated expression stored in r1 by the arrays type size
+add r7,r7,r1 % add new dimension access to current offset counter
+add r9,r9,r7 % add array dimension calculation with other properties
+ % --- compute index over ---
+subi r12,r13,36 % load stack array  offset
+add r12,r12,r9
+lw r1,0(r12) % load stack array index value in function
 jl r15,putint
 addi r1,r0,10
 putc r1
+j for1
+forend1
 hlt
+func0 % func addValueToArray entry point
+subi r12,r13,20 % store the return address in stack memory
+sw 0(r12),r11
+addi r1,r0,0 % load integer value
+subi r12,r13,-392 % load stack variable offset
+sw 0(r12),r1 % store register value into stack variable
+for2
+ % ---evaluating LT expression below ---
+subi r12,r13,-392
+lw r1,0(r12) % load stack variable value in function
+add r2,r1,r0
+subi r8,r8,4
+sw topaddr(r8),r2 % store left hand side of relational expression on stack
+addi r1,r0,5 % load integer value
+lw r2,topaddr(r8)
+addi r8,r8,4
+clt r1,r2,r1
+ % ---evaluating LT expression over ---
+bz r1,forend2
+ % ---evaluating ADD expression below ---
+subi r12,r13,-392
+lw r2,0(r12) % load stack variable value in function
+addi r1,r0,1 % load integer value
+add r1,r2,r1
+subi r8,r8,4
+sw topaddr(r8),r1
+ % ---evaluting ADD expression over ---
+lw r1,topaddr(r8)
+addi r8,r8,4 % remove stack variable from arithmetic expression
+subi r12,r13,-392 % load stack variable offset
+sw 0(r12),r1 % store register value into stack variable
+ % ---evaluating ADD expression below ---
+ % --- compute index value ---
+add r9,r0,r0 % clear array index register
+add r7,r0,r0
+subi r12,r13,-392
+lw r1,0(r12) % load stack variable value in function
+muli r7,r7,100
+muli r1,r1,4 % multiply last evaluated expression stored in r1 by the arrays type size
+add r7,r7,r1 % add new dimension access to current offset counter
+add r9,r9,r7 % add array dimension calculation with other properties
+ % --- compute index over ---
+subi r12,r13,16 % load stack array  offset
+add r12,r12,r9
+lw r2,0(r12) % load stack array index value in function
+subi r12,r13,-388
+lw r1,0(r12) % load stack variable value in function
+add r1,r2,r1
+subi r8,r8,4
+sw topaddr(r8),r1
+ % ---evaluting ADD expression over ---
+lw r1,topaddr(r8)
+addi r8,r8,4 % remove stack variable from arithmetic expression
+add r5,r0,r1
+ % --- compute index value ---
+add r9,r0,r0 % clear array index register
+add r7,r0,r0
+subi r12,r13,-392
+lw r1,0(r12) % load stack variable value in function
+muli r7,r7,100
+muli r1,r1,4 % multiply last evaluated expression stored in r1 by the arrays type size
+add r7,r7,r1 % add new dimension access to current offset counter
+add r9,r9,r7 % add array dimension calculation with other properties
+ % --- compute index over ---
+subi r12,r13,16 % load stack array offset
+lw r12,0(r12) % load array address
+add r12,r12,r9
+sw 0(r12),r5 % store register value in stack array index
+j for2
+forend2
+addi r1,r0,0 % load integer value
+subi r12,r13,20 % load return address in r12
+lw r11,0(r12)
+jr r11
 putint   add    r2,r0,r0         % c := 0 (character)
          add    r3,r0,r0         % s := 0 (sign)
          addi   r4,r0,endbuf     % p is the buffer pointer
